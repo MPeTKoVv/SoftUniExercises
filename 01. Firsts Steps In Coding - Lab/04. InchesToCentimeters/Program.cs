f6db41940d33536cd3a075b9d227be50bf470443
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-12 14:44:07 +0300

    Added exercises.

---
+++ 01. Firsts Steps In Coding - Lab/04. InchesToCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T04._Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch=double.Parse(Console.ReadLine());
+            double cm = 2.54;
+            Console.WriteLine(inch*cm);
+        }
+    }
+}
