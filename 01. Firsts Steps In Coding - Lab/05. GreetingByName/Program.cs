caafdbd42af748b56f4e291b9a1aeaea00286e34
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-12 14:46:52 +0300

    Added exercises

---
+++ 01. Firsts Steps In Coding - Lab/05. GreetingByName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Т05._Greeting_by_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name=Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ 01. Firsts Steps In Coding - Lab/08. PetShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Т08._Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PacketsOfDogFood=int.Parse(Console.ReadLine());
+            int PacketsOfCatFood=int.Parse(Console.ReadLine());
+            double DogFood = 2.5;
+            double CatFood = 4;
+            Console.WriteLine(PacketsOfDogFood * DogFood + PacketsOfCatFood * CatFood+" lv.");
+        }
+    }
+}
