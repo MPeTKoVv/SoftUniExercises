63937441eeb5d8e60fd179851321b5c9ce4e027c
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-13 11:21:14 +0300

    added lab

---
+++ Programming Basics/03. Conditional Statements - Lab/T07. Area of Figures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T07._Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure=Console.ReadLine();
+            if (figure=="square")
+            {
+                double side1=double.Parse(Console.ReadLine());
+                double area = Math.Pow(side1,2);
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (figure== "rectangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                double area = side1 * side2;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (figure== "circle")
+            {
+                double radius=double.Parse(Console.ReadLine());
+                double area = Math.PI * Math.Pow(radius, 2);
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (figure== "triangle")
+            {
+                double sidea=double.Parse(Console.ReadLine());
+                double ha=double.Parse(Console.ReadLine());
+                double area = (sidea * ha) / 2;
+                Console.WriteLine($"{area:f3}");
+
+            }
+        }
+    }
+}
