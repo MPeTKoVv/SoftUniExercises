da54dc4aa90cba4be698ad84de9d9312bf62a320
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-13 11:17:16 +0300

    added excercise

---
+++ Programming Basics/2. First Steps In Coding - Exercise/P03. DepositCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P03._DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositedAmount = double.Parse(Console.ReadLine());
+            int term = int.Parse(Console.ReadLine());
+            double percent=double.Parse(Console.ReadLine());
+            double finalAmount = depositedAmount + term * ((depositedAmount * (percent / 100)) / 12);
+            Console.WriteLine(finalAmount);
+        }
+
+    }
+}
---
+++ Programming Basics/2. First Steps In Coding - Exercise/P04.VacationBooksList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P04.VacationBooksList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int days=int.Parse(Console.ReadLine());
+            int hoursPerDay = (pages / pagesPerHour) / days;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
