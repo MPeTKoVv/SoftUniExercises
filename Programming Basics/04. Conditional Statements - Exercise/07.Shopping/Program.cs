01660f8e4cce313081fcfbbf7f2d987b56cb7758
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-14 23:58:18 +0300

    added exercise

---
+++ Programming Basics/04. Conditional Statements - Exercise/07.Shopping/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace _07.Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            int videocardsAmount = int.Parse(Console.ReadLine());
+            int cpuAmount = int.Parse(Console.ReadLine());
+            int ramAmount = int.Parse(Console.ReadLine());
+
+            decimal videocardPricePerOne = 250.0m;
+            decimal videocardPrice = videocardsAmount * videocardPricePerOne;
+            decimal cpuPrice = 0.35m * videocardPrice;
+            decimal ramPrice = 0.1m * videocardPrice;
+
+            decimal totalPrice = videocardPrice + cpuAmount * cpuPrice + ramAmount * ramPrice;
+
+            if (videocardsAmount > cpuAmount) totalPrice = totalPrice * 0.85m;
+
+            decimal neededMoney = totalPrice - budget;
+            decimal leftMoney = budget - totalPrice;
+
+            if (budget >= totalPrice) Console.WriteLine($"You have {leftMoney:f2} leva left!");
+            else if (budget < totalPrice) Console.WriteLine($"Not enough money! You need {neededMoney:f2} leva more!");
+        }
+    }
+}
---
+++ Programming Basics/04. Conditional Statements - Exercise/08.LunchBreak/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace _08.LunchBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string series = Console.ReadLine();
+            double episodeTime = double.Parse(Console.ReadLine());
+            double breakTime = double.Parse(Console.ReadLine());
+            double lunchTime = breakTime / 8;
+            double restTime = breakTime / 4;
+
+            breakTime -= (lunchTime + restTime);
+            double leftTime = breakTime - episodeTime;
+            double neededTime = Math.Abs(leftTime);
+
+            if (breakTime >= episodeTime)
+            {
+                Console.WriteLine($"You have enough time to watch {series} and left with {Math.Ceiling(leftTime)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {series}, you need {Math.Ceiling(neededTime)} more minutes.");
+            }
+        }
+    }
+}
