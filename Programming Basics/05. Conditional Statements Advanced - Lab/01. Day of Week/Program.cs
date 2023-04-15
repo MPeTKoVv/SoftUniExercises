1d4a87d99134808d3baa669e9574f086028725e9
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-15 12:01:26 +0300

    added lab

---
+++ Programming Basics/05. Conditional Statements Advanced - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfWeek = int.Parse(Console.ReadLine());
+
+            /*
+            if (dayOfWeek == 1) Console.WriteLine("Monday");
+            else if (dayOfWeek == 2) Console.WriteLine("Tuesday");
+            else if (dayOfWeek == 3) Console.WriteLine("Wednesday");
+            else if (dayOfWeek == 4) Console.WriteLine("Thursday");
+            else if (dayOfWeek == 5) Console.WriteLine("Friday");
+            else if (dayOfWeek == 6) Console.WriteLine("Saturday");
+            else if (dayOfWeek == 7) Console.WriteLine("Sunday");
+            else Console.WriteLine("Error");
+            */
+
+            switch (dayOfWeek)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
