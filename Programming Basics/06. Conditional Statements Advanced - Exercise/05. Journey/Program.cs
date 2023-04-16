c0717b4eaa7aefbe18d3eda16e6b06f32ceda69d
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-16 16:18:25 +0300

    added exercise

---
+++ Programming Basics/06. Conditional Statements Advanced - Exercise/05. Journey/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _05._Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string campOrHotel = string.Empty;
+            double spendedMoneyPercent = 0;
+            double price = 0;
+            string place = string.Empty;
+
+            if (budget > 1000)
+            {
+                place = "Europe";
+                campOrHotel = "Hotel";
+                spendedMoneyPercent = 0.9;
+            }
+            else if (budget > 100)
+            {
+                place = "Balkans";
+                if (season == "summer")
+                {
+                    campOrHotel = "Camp";
+                    spendedMoneyPercent = 0.40;
+                }
+                else
+                {
+                    campOrHotel = "Hotel";
+                    spendedMoneyPercent = 0.80;
+                }
+            }
+            else
+            {
+                place = "Bulgaria";
+                if (season == "summer")
+                {
+                    campOrHotel = "Camp";
+                    spendedMoneyPercent = 0.30;
+                }
+                else
+                {
+                    campOrHotel = "Hotel";
+                    spendedMoneyPercent = 0.70;
+                }
+            }
+            price = spendedMoneyPercent * budget;
+            Console.WriteLine($"Somewhere in {place}");
+            Console.WriteLine($"{campOrHotel} - {price:f2}");
+
+        }
+    }
+}
---
+++ Programming Basics/06. Conditional Statements Advanced - Exercise/06. Operations Between Numbers/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _06._Operations_Between_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double result = 0;
+
+            if (operation == "+")
+            {
+                result = n1 + n2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} {operation} {n2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} {operation} {n2} = {result} - odd");
+                }
+            }
+            else if (operation == "-")
+            {
+                result = n1 - n2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} {operation} {n2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} {operation} {n2} = {result} - odd");
+                }
+            }
+            else if (operation == "*")
+            {
+                result = n1 * n2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} {operation} {n2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} {operation} {n2} = {result} - odd");
+                }
+            }
+            else if (operation == "/" && n2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {n1} by zero");
+            }
+            else if (operation == "/")
+            {
+                result = n1 / n2;
+                Console.WriteLine($"{n1} {operation} {n2} = {result:f2}");
+            }
+            else if (operation == "%" && n2 == 0)
+            {
+                Console.WriteLine($"Cannot divide {n1} by zero");
+            }
+            else if (operation == "%")
+            {
+                Console.WriteLine($"{n1} % {n2} = {n1 % n2}");
+            }
+        }
+    }
+}
