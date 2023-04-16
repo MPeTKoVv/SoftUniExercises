a4ac35e79e20fb29a5e9f0d40077f13e66eab309
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-16 16:18:50 +0300

    added exercise

---
+++ Programming Basics/06. Conditional Statements Advanced - Exercise/07. Hotel Room/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _07._Hotel_Room
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int overNights = int.Parse(Console.ReadLine());
+            double apartmentPrice = 0;
+            double studioPrice = 0;
+            double totalApartmentPrice = 0;
+            double totalStudioPrice = 0;
+            double studioDiscount = 1;
+            double apartmentDiscount = 1;
+
+            if (month == "May" || month == "October")
+            {
+                apartmentPrice = 65;
+                studioPrice = 50;
+                if (overNights > 14)
+                {
+                    studioDiscount = 0.7;
+                    apartmentDiscount = 0.9;
+                }
+                else if (overNights > 7)
+                {
+                    studioDiscount = 0.95;
+                }
+            }
+            else if (month == "June" || month == "September")
+            {
+                apartmentPrice = 68.7;
+                studioPrice = 75.2;
+                if (overNights > 14)
+                {
+                    studioDiscount = 0.8;
+                    apartmentDiscount = 0.9;
+                }
+            }
+            else
+            {
+                apartmentPrice = 77;
+                studioPrice = 76;
+                if (overNights > 14)
+                {
+                    apartmentDiscount = 0.9;
+                }
+            }
+            totalApartmentPrice = overNights * apartmentPrice * apartmentDiscount;
+            totalStudioPrice = overNights * studioPrice * studioDiscount;
+            Console.WriteLine($"Apartment: {totalApartmentPrice:f2} lv.");
+            Console.WriteLine($"Studio: {totalStudioPrice:f2} lv.");
+        }
+    }
+}
---
+++ Programming Basics/06. Conditional Statements Advanced - Exercise/08. On Time for the Exam/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace _08._On_Time_for_the_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startHour = int.Parse(Console.ReadLine());
+            int startMinute = int.Parse(Console.ReadLine());
+            int arriveHour = int.Parse(Console.ReadLine());
+            int arriveMinute = int.Parse(Console.ReadLine());
+
+            if (arriveHour * 60 + arriveMinute - (startHour * 60 + startMinute) > 0)
+            {
+                int time = arriveHour * 60 + arriveMinute - (startHour * 60 + startMinute);
+                int hour = time / 60;
+                int minute = time % 60;
+                Console.WriteLine("Late");
+
+                if (minute == 0)
+                {
+                    Console.WriteLine($"{hour} 00 hours after the start");
+                }
+                else if (minute < 10 && hour >= 1)
+                {
+                    Console.WriteLine($"{hour}:0{minute} hours after the start");
+                }
+                else if (hour >= 1)
+                {
+                    Console.WriteLine($"{hour}:{minute} hours after the start");
+                }
+                else
+                {
+                    Console.WriteLine($"{minute} minutes after the start");
+                }
+            }
+            else if (startHour * 60 + startMinute == arriveHour * 60 + arriveMinute)
+            {
+                Console.WriteLine("On time");
+            }
+            else if (startHour * 60 + startMinute - (arriveHour * 60 + arriveMinute) <= 30)
+            {
+                Console.WriteLine("On time");
+                int time = startHour * 60 + startMinute - (arriveHour * 60 + arriveMinute);
+                int hour = time / 60;
+                int minute = time % 60;
+                Console.WriteLine($"{minute} minutes before the start");
+            }
+
+            else if (startHour * 60 + startMinute - (arriveHour * 60 + arriveMinute) > 30)
+            {
+                int time = startHour * 60 + startMinute - (arriveHour * 60 + arriveMinute);
+                int hour = time / 60;
+                int minute = time % 60;
+                Console.WriteLine("Early");
+
+                if (minute == 0)
+                {
+                    Console.WriteLine($"{hour} 00 hours before the start");
+                }
+                else if (minute < 10 && hour >= 1)
+                {
+                    Console.WriteLine($"{hour}:0{minute} hours before the start");
+                }
+                else if (hour >= 1)
+                {
+                    Console.WriteLine($"{hour}:{minute} hours before the start");
+                }
+                else
+                {
+                    Console.WriteLine($"{minute} minutes before the start");
+                }
+            }
+        }
+    }
+}
