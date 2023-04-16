5ac6cecfdc54be95fa464ec14387d86168ad0dd3
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-16 16:16:42 +0300

    added exercise

---
+++ Programming Basics/06. Conditional Statements Advanced - Exercise/01. Cinema/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            int totalSeats = rows * colums;
+            double price = 0;
+            switch (type)
+            {
+                case "Premiere":
+                    price = 12;
+                    break;
+                case "Normal":
+                    price = 7.5;
+                    break;
+                case "Discount":
+                    price = 5;
+                    break;
+            }
+            double totalPrice = price * totalSeats;
+            Console.WriteLine($"{totalPrice:f2} leva");
+        }
+    }
+}
---
+++ Programming Basics/06. Conditional Statements Advanced - Exercise/02. Summer Outfit/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _02._Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double degrees = double.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+            string outfit = string.Empty;
+            string shoes = string.Empty;
+            if (degrees >= 25)
+            {
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                        break;
+                    case "Afternoon":
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                        break;
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                }
+            }
+            else if (degrees > 18)
+            {
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                    case "Afternoon":
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                        break;
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                }
+            }
+            else
+            {
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                        break;
+                    case "Afternoon":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        break;
+                }
+            }
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
