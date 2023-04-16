7050ab2b528e2de928836e911b45ff9d999bb38a
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-16 16:21:14 +0300

    added lab

---
+++ Programming Basics/07. For Loop - Lab/01. Numbers from 1 to 100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Numbers_from_1_to_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
