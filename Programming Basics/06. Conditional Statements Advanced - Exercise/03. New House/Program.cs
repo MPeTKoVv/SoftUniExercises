3f0531dbfdc49eb3b14a733b43fa191174404046
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-16 16:17:25 +0300

    added exercise

---
+++ Programming Basics/06. Conditional Statements Advanced - Exercise/03. New House/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _03._New_House
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string flower = Console.ReadLine();
+            int flowerAmount = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+            double price = 0;
+            double rosePrice = 5;
+            double dahliaPrice = 3.8;
+            double tulipPrice = 2.8;
+            double narcissusPrice = 3;
+            double gladiolusPrice = 2.5;
+            double discount = 1;
+
+            if (flower == "Roses" && flowerAmount > 80)
+            {
+                discount = 0.9;
+                price = rosePrice * flowerAmount;
+            }
+            else if (flower == "Dahlias" && flowerAmount > 90)
+            {
+                discount = 0.85;
+                price = dahliaPrice * flowerAmount;
+            }
+            else if (flower == "Tulips" && flowerAmount > 80)
+            {
+                discount = 0.85;
+                price = tulipPrice * flowerAmount;
+            }
+            else if (flower == "Narcissus" && flowerAmount < 120)
+            {
+                discount = 1.15;
+                price = narcissusPrice * flowerAmount;
+            }
+            else if (flower == "Gladiolus" && flowerAmount < 80)
+            {
+                discount = 1.2;
+                price = gladiolusPrice * flowerAmount;
+            }
+            else if (flower == "Roses")
+            {
+                price = rosePrice * flowerAmount;
+            }
+            else if (flower == "Dahlias")
+            {
+                price = dahliaPrice * flowerAmount;
+            }
+            else if (flower == "Tulips")
+            {
+                price = tulipPrice * flowerAmount;
+            }
+            else if (flower == "Narcissus")
+            {
+                price = narcissusPrice * flowerAmount;
+            }
+            else if (flower == "Gladiolus")
+            {
+                price = gladiolusPrice * flowerAmount;
+            }
+            price *= discount;
+            double leftMoney = budget - price;
+            double neededMoney = price - budget;
+
+            if (budget >= price)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {flowerAmount} {flower} and {leftMoney:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {neededMoney:f2} leva more.");
+            }
+        }
+    }
+}
---
+++ Programming Basics/06. Conditional Statements Advanced - Exercise/04. Fishing Boat/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace _04._Fishing_Boat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermen = int.Parse(Console.ReadLine());
+            double price = 0;
+            double discount = 1;
+
+            if (season == "Spring")
+            {
+                price = 3000;
+
+                if (fishermen > 12)
+                {
+                    discount = 0.75;
+                }
+                else if (fishermen >= 7)
+                {
+                    discount = 0.85;
+                }
+                else
+                {
+                    discount = 0.9;
+                }
+
+            }
+            else if (season == "Summer")
+            {
+                price = 4200;
+
+                if (fishermen > 12)
+                {
+                    discount = 0.75;
+                }
+                else if (fishermen >= 7)
+                {
+                    discount = 0.85;
+                }
+                else
+                {
+                    discount = 0.9;
+                }
+            }
+            else if (season == "Autumn")
+            {
+                price = 4200;
+
+                if (fishermen > 12)
+                {
+                    discount = 0.75;
+                }
+                else if (fishermen >= 7)
+                {
+                    discount = 0.85;
+                }
+                else
+                {
+                    discount = 0.9;
+                }
+            }
+            else if (season == "Winter")
+            {
+                price = 2600;
+
+                if (fishermen > 12)
+                {
+                    discount = 0.75;
+                }
+                else if (fishermen >= 7)
+                {
+                    discount = 0.85;
+                }
+                else
+                {
+                    discount = 0.9;
+                }
+            }
+
+            //switch (season)
+            //{
+            //    case "Spring":
+            //        price = 3000;
+            //        break;
+            //    case "Summer":
+            //    case "Autumn":
+            //        price = 4200;
+            //        break;
+            //    case "Winter":
+            //        price = 2600;
+            //        break;
+            //}
+            //if (fishermen > 12)
+            //{
+            //    discount = 0.75;
+            //}
+            //else if (fishermen >= 7)
+            //{
+            //    discount = 0.85;
+            //}
+            //else
+            //{
+            //    discount = 0.9;
+            //}
+
+            price *= discount;
+            if (fishermen % 2 == 0 && season != "Autumn")
+            {
+                discount = 0.95;
+                price *= discount;
+            }
+            double leftMoney = budget - price;
+            double neededMoney = Math.Abs(leftMoney);
+
+            if (budget >= price)
+            {
+                Console.WriteLine($"Yes! You have {leftMoney:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {neededMoney:f2} leva.");
+            }
+        }
+    }
+}
