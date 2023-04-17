a1453b70bbd819c83572093cf3105192541fd87e
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-17 11:04:47 +0300

    added lab

---
+++ Programming Basics/07. For Loop - Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05._Character_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word=Console.ReadLine();
+            int lengthOfWord=word.Length;
+            for (int index = 0; index < lengthOfWord; index++)
+            {
+                char currentChar = word[index];
+                Console.WriteLine(currentChar);
+            }
+        }
+    }
+}
