295b4d1430d48e9f4c430825c3b5297491fdd32c
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-17 11:05:28 +0300

    added lab

---
+++ Programming Basics/07. For Loop - Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+            for (int counter = 0; counter < countOfInputs; counter++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (counter % 2 == 0)
+                {
+                    evenSum += num;
+                }
+                else
+                {
+                    oddSum += num;
+                }
+            }
+            if (evenSum==oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }    
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(oddSum-evenSum)}");
+            }
+        }
+    }
+}
