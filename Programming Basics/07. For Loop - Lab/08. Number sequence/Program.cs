492cb5560672fdf7e280bfe36da0fa3bbced7711
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-17 11:05:17 +0300

    added lab

---
+++ Programming Basics/07. For Loop - Lab/08. Number sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08._Number_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+            for (int i = 0; i < countOfInputs; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
---
+++ Programming Basics/07. For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            int rightSum = 0;
+            int leftSum = 0;
+            for (int counter = 0; counter < 2*countOfInputs; counter++)
+            {
+                int currentNumber=int.Parse(Console.ReadLine());
+                if (counter < countOfInputs)
+                {
+                    rightSum+=currentNumber;
+                }
+                else
+                {
+                    leftSum+=currentNumber;
+                }
+            }
+            if (rightSum == leftSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+            }
+
+        }
+    }
+}
