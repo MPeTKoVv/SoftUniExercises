79f86ce5f6ffc368c79a47c2ab7333e42d9c490c
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-17 11:05:02 +0300

    added lab

---
+++ Programming Basics/07. For Loop - Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._Vowels_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int lengthOfWord=word.Length;
+            int sum = 0;
+            for (int i = 0; i < lengthOfWord; i++)
+            {
+                char currentChar=word[i];
+                switch (currentChar)
+                {
+                    case 'a':
+                        sum += 1;
+                        break;
+                    case 'e':
+                        sum += 2;
+                        break;
+                    case 'i':
+                        sum += 3;
+                        break;
+                    case 'o':
+                        sum += 4;
+                        break;
+                    case 'u':
+                        sum += 5;
+                        break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
