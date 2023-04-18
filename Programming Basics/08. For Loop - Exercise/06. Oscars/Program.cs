4c330358a641b45dc359d566ac38718605bd86ce
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-18 19:03:20 +0300

    added exercise

---
+++ Programming Basics/08. For Loop - Exercise/06. Oscars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double pointsFromAcademy = double.Parse(Console.ReadLine());
+            double amountOfPanel = double.Parse(Console.ReadLine());
+            double totalPoints = pointsFromAcademy;
+            for (int counter = 1; counter <= amountOfPanel; counter++)
+            {
+                string appraisal = Console.ReadLine();
+                double pointsFromAppraisal = double.Parse(Console.ReadLine());
+                double length = appraisal.Length;
+                totalPoints += pointsFromAppraisal * length / 2;
+                if (totalPoints > 1250.5)
+                {
+                    break;
+                }
+            }
+            if (totalPoints > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {name} got a nominee for leading role with {totalPoints:f1}!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {name} you need {1250.5 - totalPoints:f1} more!");
+            }
+        }
+    }
+}
---
+++ Programming Basics/08. For Loop - Exercise/07. Trekking Mania/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _07._Trekking_Mania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfGroups = int.Parse(Console.ReadLine());
+            double hikingMusala = 0;
+            double hikingMonbat = 0;
+            double hikingKilimandjaro = 0;
+            double hikingK2 = 0;
+            double hikingEverest = 0;
+            double totalPeople = 0;
+            for (int counter = 1; counter <= amountOfGroups; counter++)
+            {
+                double peopleInGroup = double.Parse(Console.ReadLine());
+                if (peopleInGroup <= 5)
+                {
+                    hikingMusala += peopleInGroup;
+                    totalPeople += peopleInGroup;
+                }
+                else if (peopleInGroup <= 12)
+                {
+                    hikingMonbat += peopleInGroup;
+                    totalPeople += peopleInGroup;
+                }
+
+                else if (peopleInGroup <= 25)
+                {
+                    hikingKilimandjaro += peopleInGroup;
+                    totalPeople += peopleInGroup;
+                }
+                else if (peopleInGroup <= 40)
+                {
+                    hikingK2 += peopleInGroup;
+                    totalPeople += peopleInGroup;
+                }
+                else
+                {
+                    hikingEverest += peopleInGroup;
+                    totalPeople += peopleInGroup;
+                }
+
+            }
+            Console.WriteLine($"{hikingMusala / totalPeople * 100:f2}%");
+            Console.WriteLine($"{hikingMonbat / totalPeople * 100:f2}%");
+            Console.WriteLine($"{hikingKilimandjaro / totalPeople * 100:f2}%");
+            Console.WriteLine($"{hikingK2 / totalPeople * 100:f2}%");
+            Console.WriteLine($"{hikingEverest / totalPeople * 100:f2}%");
+        }
+    }
+}
