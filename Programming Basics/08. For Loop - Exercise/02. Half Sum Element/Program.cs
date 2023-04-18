611ef7d009e2db96e9bbc35d1baea1530f191996
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-18 19:02:52 +0300

    added exercise

---
+++ Programming Basics/08. For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfInputs = int.Parse(Console.ReadLine());
+            int maxValue = int.MinValue;
+            int sum = 0;
+            for (int counter = 0; counter < amountOfInputs; counter++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+                if (currentNum > maxValue)
+                {
+                    maxValue = currentNum;
+                }
+            }
+
+            if (sum - maxValue == maxValue)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxValue}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sum - 2 * maxValue)}");
+            }
+        }
+    }
+}
---
+++ Programming Basics/08. For Loop - Exercise/03. Histogram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03._Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+            for (int counter = 1; counter <= n; counter++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber < 200) p1++;
+                else if (currentNumber < 400) p2++;
+                else if (currentNumber < 600) p3++;
+                else if (currentNumber < 800) p4++;
+                else p5++;
+
+            }
+            Console.WriteLine($"{p1 / n * 100:f2}%");
+            Console.WriteLine($"{p2 / n * 100:f2}%");
+            Console.WriteLine($"{p3 / n * 100:f2}%");
+            Console.WriteLine($"{p4 / n * 100:f2}%");
+            Console.WriteLine($"{p5 / n * 100:f2}%");
+        }
+    }
+}
