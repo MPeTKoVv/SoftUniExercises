0b347a418ebafd7565699ba2e17646ef84ec0993
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-19 15:51:12 +0300

    added lab

---
+++ Programming Basics/09. While Loop - Lab/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Sequence_2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = 1;
+            Console.WriteLine(k);
+
+            while (2 * k + 1 <= n)
+            {
+                k = 2 * k + 1;
+                Console.WriteLine(k);
+            }
+        }
+    }
+}
