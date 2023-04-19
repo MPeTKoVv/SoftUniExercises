67f56f86335d4e6b99a61a649868eab9b974f486
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-19 15:51:54 +0300

    added lab

---
+++ Programming Basics/09. While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int minNum = int.MaxValue;
+            while (input != "Stop")
+            {
+                int inputAsNumber = int.Parse(input);
+                if (inputAsNumber < minNum)
+                {
+                    minNum = inputAsNumber;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
