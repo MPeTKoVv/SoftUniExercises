57c0bdef23efc6d6c2cf540d9188628bacf10680
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-19 15:52:32 +0300

    added lab

---
+++ Programming Basics/09. While Loop - Lab/08. Graduation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _08._Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int badGrades = 0;
+            int classCnt = 0;
+            double gradeSum = 0;
+            bool isThePersonExcluded = false;
+            double averageGrade = 0;
+            while (classCnt < 12)
+            {
+                classCnt++;
+                double grade = double.Parse(Console.ReadLine());
+                if (grade < 4)
+                {
+                    badGrades++;
+                }
+                if (badGrades == 2)
+                {
+                    classCnt--;
+                    isThePersonExcluded = true;
+                    break;
+                }
+                gradeSum += grade;
+                averageGrade = gradeSum / classCnt;
+            }
+            if (isThePersonExcluded)
+            {
+                Console.WriteLine($"{name} has been excluded at {classCnt} grade");
+            }
+            else
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+            }
+        }
+    }
+}
