364b1a83c32cb21fb300ab103030bf460bb877e9
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-19 15:51:42 +0300

    added lab

---
+++ Programming Basics/09. While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int maxNum = int.MinValue;
+            while (input != "Stop")
+            {
+                int inputAsNumber = int.Parse(input);
+                if (inputAsNumber > maxNum)
+                {
+                    maxNum = inputAsNumber;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
