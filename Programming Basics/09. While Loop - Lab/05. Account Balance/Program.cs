a35e644abd8ce32cce7b83111d3615045fbcaa3f
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-19 15:51:28 +0300

    added lab

---
+++ Programming Basics/09. While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            string input = Console.ReadLine();
+
+            while (input != "NoMoreMoney")
+            {
+                if (input == "NoMoreMoney")
+                {
+                    break;
+                }
+                double inputAsNumber = double.Parse(input);
+                if (inputAsNumber < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                total += inputAsNumber;
+                Console.WriteLine($"Increase: {inputAsNumber:f2}");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {total:f2}");
+        }
+    }
+}
