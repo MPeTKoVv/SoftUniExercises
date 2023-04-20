a3f779a6a473823fca71015e0e5ddb587988e7c9
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-20 17:47:03 +0300

    added lab

---
+++ Programming Basics/10. While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int size = a * b;
+            int neededPieces = 0;
+            int leftPieces;
+            bool isStopped = false;
+            leftPieces = size;
+            while (leftPieces >= 0)
+            {
+
+                string currentPiece = Console.ReadLine();
+                if (currentPiece == "STOP")
+                {
+                    isStopped = true;
+                    break;
+                }
+
+                if (int.Parse(currentPiece) > leftPieces)
+                {
+                    neededPieces = int.Parse(currentPiece) - leftPieces;
+                }
+
+                leftPieces -= int.Parse(currentPiece);
+            }
+            if (isStopped)
+            {
+                Console.WriteLine($"{leftPieces} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {neededPieces} pieces more.");
+            }
+        }
+    }
+}
