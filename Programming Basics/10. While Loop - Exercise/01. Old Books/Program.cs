c926dbf4a25586d570c3337113deab384f99f900
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-20 17:46:31 +0300

    added lab

---
+++ Programming Basics/10. While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedBook = Console.ReadLine();
+            int checkedBooks = 0;
+            string currentBook;
+            while ((currentBook = Console.ReadLine()) != searchedBook)
+            {
+                if (currentBook == "No More Books")
+                {
+                    break;
+                }
+                checkedBooks++;
+            }
+            if (currentBook == searchedBook)
+            {
+                Console.WriteLine($"You checked {checkedBooks} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {checkedBooks} books.");
+            }
+        }
+    }
+}
