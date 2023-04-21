72998d7c55259ca8ccf41a6aa6c1a6c90dbacee8
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-21 21:28:00 +0300

    added lab

---
+++ Programming Basics/11. Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            bool isColect = false;
+
+            while (destination != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+                while (savedMoney < budget)
+                {
+                    double currentMoney = double.Parse(Console.ReadLine());
+                    savedMoney += currentMoney;
+                    if (savedMoney >= budget)
+                    {
+                        isColect = true;
+                        break;
+                    }
+                }
+                if (isColect)
+                {
+                    Console.WriteLine($"Going to {destination}!");
+                    isColect = false;
+                }
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
