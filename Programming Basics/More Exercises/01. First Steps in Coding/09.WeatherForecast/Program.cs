15399b5d01aba9d24cbbe2514e9a24cf3515ffbd
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-24 00:03:02 +0300

    Apprentice

---
+++ Programming Basics/More Exercises/01. First Steps in Coding/09.WeatherForecast/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09.WeatherForecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strinG = Console.ReadLine();
+
+            if (strinG == "sunny")
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+
+        }
+    }
+}
