e753bd3e901c9abb3bb8d2c7736c9a115fcf9608
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-25 16:23:12 +0300

    added lab

---
+++ Programming Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours++;
+            }
+            if (hours >= 24)
+            {
+                hours -= 24;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
