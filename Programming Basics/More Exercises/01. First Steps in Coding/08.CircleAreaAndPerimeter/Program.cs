2f42c730d3cea7aa8658aafeb663dcbc9e3f44f6
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-25 16:33:29 +0300

    added more exercise

---
+++ Programming Basics/More Exercises/01. First Steps in Coding/08.CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _08.CircleAreaAndPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(r, 2);
+            double parameter = 2 * Math.PI * r;
+            Console.WriteLine($"{area:f2}");
+            Console.WriteLine($"{parameter:f2}");
+
+        }
+    }
+}
