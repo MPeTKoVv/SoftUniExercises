d2fe989933047562801e2b5f37a06b76eb32125a
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-26 21:03:53 +0300

    added lab

---
+++ Programming Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1>10)
+            {
+                Console.WriteLine($"{num} X {num1} = {num*num1}");
+            }
+
+            for (int i = num1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} X {i} = {num * i}");
+            }
+        }
+    }
+}
