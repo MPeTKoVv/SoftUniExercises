36b42de633560e6af7c108cfc9f6ea19850fe6c5
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-27 20:26:23 +0300

    added exercise

---
+++ Programming Fundamentals/02. Basic Syntax, Conditional Statements and Loops - Exercise/06. Strong number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int num1 = num;
+
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int factorialNum = 1;
+                int currNum = num % 10;
+                num /= 10;
+
+                for (int i = 2; i <= currNum; i++)
+                {
+                    factorialNum *= i;
+                }
+
+                sum += factorialNum;
+            }
+
+            Console.WriteLine(sum == num1 ? "yes" : "no");
+        }
+    }
+}
