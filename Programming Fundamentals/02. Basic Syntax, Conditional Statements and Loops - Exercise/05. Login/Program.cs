628864dbec3a31820c3fc98de8e38296dd54c161
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-27 20:26:17 +0300

    added exercise

---
+++ Programming Fundamentals/02. Basic Syntax, Conditional Statements and Loops - Exercise/05. Login/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05._Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string correctpassword = "";
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                correctpassword += username[i];
+            }
+
+            string password = Console.ReadLine();
+
+            int cnt = 0;
+
+            while (password != correctpassword)
+            {
+                cnt++;
+                if (cnt == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+                password = Console.ReadLine();
+            }
+
+            Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
