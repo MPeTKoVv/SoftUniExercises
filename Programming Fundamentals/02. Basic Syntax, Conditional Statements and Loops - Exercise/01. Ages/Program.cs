53eb62ae267f39f7ec6b0bebd8eebd2adc530fe8
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-27 20:25:37 +0300

    added exercise

---
+++ Programming Fundamentals/02. Basic Syntax, Conditional Statements and Loops - Exercise/01. Ages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string person = "";
+
+            if (age >= 0 && age <= 2)
+            {
+                person = "baby";
+            }
+            else if (age < 14)
+            {
+                person = "child";
+            }
+            else if (age < 20)
+            {
+                person = "teenager";
+            }
+            else if (age < 66)
+            {
+                person = "adult";
+            }
+            else if (age >= 66)
+            {
+                person = "elder";
+            }
+
+            Console.WriteLine(person);
+        }
+    }
+}
