d1c7e6c932581d36a6d9c545a7d3138b8d2ff25b
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-28 15:03:01 +0300

    added exercise

---
+++ Programming Fundamentals/02. Basic Syntax, Conditional Statements and Loops - Exercise/11. Orders/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+
+            double price = 0;
+            double totalPrice = 0;
+
+            for (int i = 0; i < orders; i++)
+            {
+
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                price = (days * capsulesCount) * capsulePrice;
+                totalPrice += price;
+
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+
+        }
+    }
+}
+
