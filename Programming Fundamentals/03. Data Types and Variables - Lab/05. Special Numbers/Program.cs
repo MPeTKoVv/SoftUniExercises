6e981a611001fccdba7dc8cfce87f357e207f176
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-29 11:14:15 +0300

    added lab

---
+++ Programming Fundamentals/03. Data Types and Variables - Lab/05. Special Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int sumOfDigits = 0;
+                int currentNum = i;
+
+                while (currentNum > 0)
+                {
+                    sumOfDigits += currentNum % 10;
+                    currentNum /= 10;
+                }
+
+                Console.WriteLine($"{i} -> {sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11}");
+            }
+        }
+    }
+}
