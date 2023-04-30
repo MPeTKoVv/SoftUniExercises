105c879a4b4fe05c28f06d5009438a4b01b9f6f2
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-30 11:13:00 +0300

    added lab

---
+++ Programming Fundamentals/03. Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                bool IsTheSumEqualToFiveSevenOrEleven = false;
+                int totalSum = 0;
+                int currentNum = i;
+
+                while (currentNum > 0)
+                {
+                    totalSum += currentNum % 10;
+                    currentNum /= 10;
+                }
+
+                IsTheSumEqualToFiveSevenOrEleven = (totalSum == 5) || (totalSum == 7) || (totalSum == 11);
+
+                Console.WriteLine("{0} -> {1}", i, IsTheSumEqualToFiveSevenOrEleven);
+            }
+        }
+    }
+}
