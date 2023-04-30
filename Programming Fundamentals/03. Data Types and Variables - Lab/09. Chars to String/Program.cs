5c67b70dc206fdd93833b95b5fd8c2f233bb6338
MPeTKoVv <petkov.martin19@gmail.com>
2023-04-30 11:12:34 +0300

    added lab

---
+++ Programming Fundamentals/03. Data Types and Variables - Lab/09. Chars to String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _09._Chars_to_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            char char3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{char1}{char2}{char3}");
+        }
+    }
+}
