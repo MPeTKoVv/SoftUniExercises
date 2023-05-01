3cd6c671d9c22b452ed69e82453faed73c699141
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-01 13:00:47 +0300

    added exercise

---
+++ Programming Fundamentals/04. Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while (num != 0)
+            {
+                int currDigit = num % 10;
+                sum += currDigit;
+                num /= 10;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
