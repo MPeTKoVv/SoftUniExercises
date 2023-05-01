c9269b0d66cca26cd0b3a368cd589e57709fb971
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-01 13:01:06 +0300

    added exercise

---
+++ Programming Fundamentals/04. Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double persons = double.Parse(Console.ReadLine());
+            //double capacity = double.Parse(Console.ReadLine());
+
+            //double courses = Math.Ceiling(persons / capacity);
+
+            //Console.WriteLine(courses);
+
+            int persons = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)persons / capacity);
+
+            Console.WriteLine(courses);
+        }
+    }
+}
