6ab40ebcee54348a7b56c12f853be8f9b96bb312
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-02 15:25:30 +0300

    added exercise

---
+++ Programming Fundamentals/04. Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxValue = double.MinValue;
+            string biggestKeg = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double currValue = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (currValue > maxValue)
+                {
+                    maxValue = currValue;
+                    biggestKeg = model;
+                }
+            }
+
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
