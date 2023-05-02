d411140f68b9e471960ae417c3b2aec88c599166
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-02 15:25:22 +0300

    added exercise

---
+++ Programming Fundamentals/04. Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CAPACITY = 255;
+
+            int cnt = int.Parse(Console.ReadLine());
+            int totalPouredWater = 0;
+
+
+            for (int i = 0; i < cnt; i++)
+            {
+                int litters = int.Parse((Console.ReadLine()));
+
+                if (litters <= CAPACITY - totalPouredWater)
+                {
+                    totalPouredWater += litters;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+            }
+
+            Console.WriteLine(totalPouredWater);
+        }
+    }
+}
