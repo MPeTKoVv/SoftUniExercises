cc951ad354dece86b34bdfddd4565435879197d8
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-02 15:25:58 +0300

    added exercise

---
+++ Programming Fundamentals/04. Data Types and Variables - Exercise/11. Snowballs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11._Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger madeSnowballs = int.Parse(Console.ReadLine());
+            BigInteger highestSnowballSnow = int.MinValue;
+            BigInteger highestSnowballTime = int.MinValue;
+            BigInteger highestSnowballQuality = int.MinValue;
+            BigInteger highestSnowballValue = int.MinValue;
+
+            for (int i = 0; i < madeSnowballs; i++)
+            {
+                BigInteger snowballSnow = int.Parse(Console.ReadLine());
+                BigInteger snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+
+                if (snowballValue >= highestSnowballValue)
+                {
+                    highestSnowballSnow = snowballSnow;
+                    highestSnowballTime = snowballTime;
+                    highestSnowballQuality = snowballQuality;
+                    highestSnowballValue = snowballValue;
+                }
+            }
+
+            Console.WriteLine($"{highestSnowballSnow} : {highestSnowballTime} = {highestSnowballValue} ({highestSnowballQuality})");
+        }
+    }
+}
