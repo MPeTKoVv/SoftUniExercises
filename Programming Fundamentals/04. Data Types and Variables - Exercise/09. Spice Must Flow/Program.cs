75cbe61364c6212af1149df49fa5cabbd3b2a9f0
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-02 15:25:46 +0300

    added exercise

---
+++ Programming Fundamentals/04. Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int days = 0;
+            int extractedAmount = 0;
+
+            while (yield >= 100)
+            {
+                extractedAmount += yield;
+                extractedAmount -= 26;
+                yield -= 10;
+                days++;
+
+                if (yield<100)
+                {
+                    extractedAmount -= 26;
+                }
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(extractedAmount);
+        }
+    }
+}
