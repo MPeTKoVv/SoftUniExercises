b5f15a77d1f54908a841ab37485ec59440de1cd9
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-02 15:25:51 +0300

    added exercise

---
+++ Programming Fundamentals/04. Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine()); //n
+            int distance = int.Parse(Console.ReadLine()); //m
+            int exhaustionFactor = int.Parse(Console.ReadLine()); //y
+
+            int originalPowerValue = power;
+            int countOfTargets = 0;
+
+            while (power >= distance)
+            {
+                power -= distance;
+                countOfTargets++;
+
+                if (power * 2 == originalPowerValue && exhaustionFactor > 0)
+                {
+                    power /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(power);
+            Console.WriteLine(countOfTargets);
+        }
+    }
+}
