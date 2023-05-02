ffb70a3669ff48bbb43886536211d1a37ccfc366
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-02 15:25:14 +0300

    added exercise

---
+++ Programming Fundamentals/04. Data Types and Variables - Exercise/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = 96 + startNum;
+
+            for (int i = 97; i <= endNum; i++)
+            {
+                for (int j = 97; j <= endNum; j++)
+                {
+                    for (int k = 97; k <= endNum; k++)
+                    {
+                        Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+                    }
+                }
+            }
+        }
+    }
+}
