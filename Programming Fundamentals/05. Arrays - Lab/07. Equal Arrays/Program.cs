cb6d9ba089145d3b33a40e8fdccdae42e38f4341
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-03 16:22:14 +0300

    added lab

---
+++ Programming Fundamentals/05. Arrays - Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+            int[] array2 = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Arrays are identical. Sum: {array1.Sum()}");
+        }
+    }
+}
---
+++ Programming Fundamentals/05. Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+            while (array.Length > 1)
+            {
+                int[] array2 = new int[array.Length-1];
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    array2[i] = array[i] + array[i + 1];
+                }
+
+                array = array2;
+            }
+
+            Console.WriteLine(array[0]);
+        }
+    }
+}
