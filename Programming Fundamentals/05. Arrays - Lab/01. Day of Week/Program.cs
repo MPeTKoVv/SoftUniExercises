ad9af163d96312e3af82bf3c0183c4e80e3b4144
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-03 16:19:37 +0300

    added lab

---
+++ Programming Fundamentals/05. Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfWeek = int.Parse(Console.ReadLine());
+
+            string[] days =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if (dayOfWeek >= 1 && dayOfWeek <= 7)
+            {
+                Console.WriteLine(days[dayOfWeek - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
