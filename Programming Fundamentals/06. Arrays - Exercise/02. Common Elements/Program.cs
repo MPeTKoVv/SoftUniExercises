927e028c70c8b398fa1b05cf1d817bbd562c6d1c
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-04 15:39:35 +0300

    added exercise

---
+++ Programming Fundamentals/06. Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            string[] input1Array = input1.Split(" ");
+            string[] input2Array = input2.Split(" ");
+
+            foreach (string currentIndexIninput2Array in input2Array)
+            {
+                foreach (string currentIndexIninput1Array in input1Array)
+                {
+                    if (currentIndexIninput2Array == currentIndexIninput1Array)
+                    {
+                        Console.Write($"{currentIndexIninput2Array} ");
+                    }
+                }
+            }
+        }
+    }
+}
