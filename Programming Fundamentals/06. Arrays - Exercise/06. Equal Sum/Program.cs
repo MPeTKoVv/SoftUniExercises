e80e5fc51e47e25556415b2b38dd050e38402514
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-04 15:42:09 +0300

    added exercise

---
+++ Programming Fundamentals/06. Arrays - Exercise/06. Equal Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            string output = "no";
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += numbers[j];
+                }
+                for (int k = i + 1; k < numbers.Length; k++)
+                {
+                    rightSum += numbers[k];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    output = i.ToString();
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
