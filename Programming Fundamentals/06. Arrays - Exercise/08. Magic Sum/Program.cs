59cfea789d75e4b72ea92183dc198c9aaed3231d
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-04 15:42:18 +0300

    added exercise

---
+++ Programming Fundamentals/06. Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = int.Parse(Console.ReadLine());
+
+            for (var num1 = 0; num1 < numbers.Length; num1++)
+            {
+                for (var num2 = num1 + 1; num2 < numbers.Length; num2++)
+                {
+                    if (numbers[num1] + numbers[num2] == sum)
+                    {
+                        Console.WriteLine($"{numbers[num1]} {numbers[num2]}");
+                    }
+                }
+            }
+        }
+    }
+}
