018faebe49da60b242ceec60dc2f0c5d1f4d29d7
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-04 15:39:27 +0300

    added exercise

---
+++ Programming Fundamentals/06. Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+
+            int[] passengers = new int[wagons];
+            int totalPassengers = 0;
+
+            for (int i = 0; i < passengers.Length; i++)
+            {
+                passengers[i] = int.Parse(Console.ReadLine());
+                totalPassengers += passengers[i];
+            }
+
+            Console.WriteLine(string.Join(" ", passengers));
+            Console.WriteLine(totalPassengers);
+        }
+    }
+}
