71abcaa92c44f1c140b309f8186b132c68a1c56a
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-05 22:17:17 +0300

    added lab

---
+++ Programming Fundamentals/07. Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            UpperPart(num);
+            Height(num);
+            DownPart(num);
+        }
+
+        private static void DownPart(int num)
+        {
+            for (int i = num; i >= 1; i--)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Height(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void UpperPart(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
