a49806d07304d5a16286301a84382a34700fa47d
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-05 22:17:03 +0300

    added lab

---
+++ Programming Fundamentals/07. Methods - Lab/02. Grades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            Grade(grade);
+        }
+
+        private static void Grade(double grade)
+        {
+            if (grade >= 5.5)
+            {
+                Console.WriteLine("Excellent");
+            }
+            else if (grade >= 4.5)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade >= 3.5)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 3)
+            {
+                Console.WriteLine("Poor");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
