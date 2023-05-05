4e8a24951275bc93ab51e64ce4ddcbb7f06392f8
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-05 22:17:10 +0300

    added lab

---
+++ Programming Fundamentals/07. Methods - Lab/03. Calculations/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine();
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (action == "add")
+            {
+                Add(num1, num2);
+            }
+            else if (action == "multiply")
+            {
+                Multiply(num1, num2);
+            }
+            else if (action == "subtract")
+            {
+                Subtract(num1, num2);
+            }
+            else
+            {
+                Devide(num1, num2);
+            }
+        }
+
+        static void Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            Console.WriteLine(result);
+        }
+        static void Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+
+            Console.WriteLine(result);
+        }
+        static void Subtract(int num1, int num2)
+        {
+            int result = num1 - num2;
+
+            Console.WriteLine(result);
+        }
+        static void Devide(int num1, int num2)
+        {
+            int result = num1 / num2;
+
+            Console.WriteLine(result);
+        }
+    }
+}
