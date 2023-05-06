ae0792b6b0f7cb254430179c169d4086931687bf
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-06 17:05:48 +0300

    added lab

---
+++ Programming Fundamentals/07. Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculate(num1, operation, num2));
+        }
+
+        static double Calculate(double num1, string operation, double num2)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
