485f9732ba2a6f67fca3c29c8b82e04a27d1b2f7
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-06 17:05:29 +0300

    added lab

---
+++ Programming Fundamentals/07. Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double @base = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(RaiseToPower(@base, power));
+        }
+
+
+        private static double RaiseToPower(double @base, double power)
+        {
+            double result = Math.Pow(@base, power);
+
+            return result;
+        }
+    }
+}
