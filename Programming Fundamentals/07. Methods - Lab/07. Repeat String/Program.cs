edabebf1c092eb4ed21c42cfbd3f8adfb3f8b558
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-06 17:05:22 +0300

    added lab

---
+++ Programming Fundamentals/07. Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int repeatedTimes = int.Parse(Console.ReadLine());
+
+            Repeat(input, repeatedTimes);
+        }
+
+        private static void Repeat(string input, int repeatedTimes)
+        {
+            for (int i = 0; i < repeatedTimes; i++)
+            {
+                Console.Write(input);
+            }
+        }
+    }
+}
