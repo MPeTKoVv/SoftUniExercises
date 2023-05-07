f329f2f6c9585b01f840b072877ef2bb60e940e0
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-07 16:24:37 +0300

    add exercise

---
+++ Programming Fundamentals/08. Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+
+            SearchForVoweles(input);
+        }
+
+        private static void SearchForVoweles(string input)
+        {
+            int cnt = 0;
+
+            foreach (char vowel in input)
+            {
+                if ("aouei".Contains(vowel))
+                {
+                    cnt++;
+                }
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
