65a62c78fad0911808525a2a615f83e6373973a2
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-07 16:24:45 +0300

    add exercise

---
+++ Programming Fundamentals/08. Methods - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            TheRange(firstChar, secondChar);
+        }
+
+        private static void TheRange(char firstChar, char secondChar)
+        {
+            int startChar = Math.Min(firstChar, secondChar);
+            int endChar = Math.Max(firstChar, secondChar);
+
+            for (int i = startChar + 1; i < endChar; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
