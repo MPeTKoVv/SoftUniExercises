5acb62400aa61da03f9486103dbaf2a433c695eb
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-08 12:58:24 +0300

    added exercise

---
+++ Programming Fundamentals/08. Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Matrix(num);
+        }
+
+        private static void Matrix(int num)
+        {
+            for (int rows = 0; rows < num; rows++)
+            {
+                for (int cols = 0; cols < num; cols++)
+                {
+                    Console.Write($"{num} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
