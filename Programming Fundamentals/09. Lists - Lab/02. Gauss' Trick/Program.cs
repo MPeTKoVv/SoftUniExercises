2b5ce5c21301f63c65248736a116d1415ba66b7e
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-09 17:24:54 +0300

    added lab

---
+++ Programming Fundamentals/09. Lists - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+            .Split()
+            .Select(double.Parse)
+            .ToList();
+
+            int originalLenght = numbers.Count;
+
+            for (int i = 0; i < originalLenght / 2; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
