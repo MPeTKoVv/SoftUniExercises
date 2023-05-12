bb9f15c8e87ab4c6ba421f4a92710e22fc541de7
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-12 22:24:14 +0300

    added lab

---
+++ Programming Fundamentals/11. Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+
+            for (int i = num; i > 1; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
