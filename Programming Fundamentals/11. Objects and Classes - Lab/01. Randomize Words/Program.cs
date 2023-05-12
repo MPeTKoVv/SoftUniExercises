9f870962818064a30d4411222986fce87efd832d
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-12 22:24:05 +0300

    added lab

---
+++ Programming Fundamentals/11. Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int rndIndex = rnd.Next(0, input.Length);
+
+                string currWord = input[i];
+                input[i] = input[rndIndex];
+                input[rndIndex] = currWord;
+            }
+
+            foreach (var item in input)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
