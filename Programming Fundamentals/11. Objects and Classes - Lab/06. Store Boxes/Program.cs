19a658e43f6c752bf0f454579e3791d39b16c1c3
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-13 15:22:30 +0300

    added lab

---
+++ Programming Fundamentals/11. Objects and Classes - Lab/06. Store Boxes/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _06._Store_Boxes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split().ToArray();
+
+                int serialNumber = int.Parse(tokens[0]);
+                string itemName = tokens[1];
+                int itemQuantity = int.Parse(tokens[2]);
+                double itemPrice = double.Parse(tokens[3]);
+
+                Item item = new Item
+                {
+                    Name = itemName,
+                    Price = itemPrice,
+                };
+
+                Box box = new Box
+                {
+                    SerialNumber = serialNumber,
+                    Item = item,
+                    ItemQuantity = itemQuantity,
+                    Price = item.Price * itemQuantity
+                };
+
+                boxes.Add(box);
+            }
+
+            boxes = boxes.OrderByDescending(b => b.Price).ToList();
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.Price:f2}");
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+
+    public class Box
+    {
+        public int SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public double Price { get; set; }
+    }
+}
