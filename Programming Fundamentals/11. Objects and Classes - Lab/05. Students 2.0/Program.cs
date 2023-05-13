b036af839fefe1def350c7087a419b24cc0505b3
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-13 15:22:08 +0300

    added lab

---
+++ Programming Fundamentals/11. Objects and Classes - Lab/05. Students 2.0/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _05._Students_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                string studentInfo = Console.ReadLine();
+
+                if (studentInfo == "end")
+                {
+                    break;
+                }
+
+                string[] tokens = studentInfo.Split().ToArray();
+
+                string firsName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+                string city = tokens[3];
+
+                Student student = new Student
+                {
+                    FirstName = firsName,
+                    LastName = lastName,
+                    Age = age,
+                    City = city
+                };
+
+
+                if (students.Any(s => s.FirstName == student.FirstName && s.LastName == s.LastName))
+                {
+                    Student studentsForRemove = students.Find(s => s.FirstName == student.FirstName && s.LastName == s.LastName);
+                    students.Remove(studentsForRemove);
+                }
+
+                students.Add(student);
+            }
+
+            string filterCity = Console.ReadLine();
+
+            var filteredStudents = students
+                .Where(s => s.City == filterCity)
+                .ToList();
+
+            foreach (var item in filteredStudents)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName} is {item.Age} years old.");
+            }
+        }
+
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
