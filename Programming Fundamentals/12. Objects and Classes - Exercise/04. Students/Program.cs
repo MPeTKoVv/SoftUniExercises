b45134722973e2b09e9b4f17ade40854e40d7073
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-14 11:41:44 +0300

    added exercise

---
+++ Programming Fundamentals/12. Objects and Classes - Exercise/04. Students/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _04._Students
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            int cnt = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cnt; i++)
+            {
+                string[] studentInfo = Console.ReadLine().Split().ToArray();
+
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                double grade = double.Parse(studentInfo[2]);
+
+                Student student = new Student(firstName, lastName, grade);
+
+                students.Add(student);
+            }
+
+            students = students.OrderByDescending(s => s.Grade).ToList();
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+
+    public class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+    }
+}
