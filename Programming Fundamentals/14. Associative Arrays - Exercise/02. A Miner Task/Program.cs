5845fb3a3edaf526c36b79e04f9614f4bb112573
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-17 22:11:24 +0300

    added exercise

---
+++ Programming Fundamentals/14. Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            string word = Console.ReadLine();
+
+            while (word != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(word))
+                {
+                    resources.Add(word, 0);
+                }
+
+                resources[word] += quantity;
+
+                word = Console.ReadLine();
+            }
+
+            foreach (var item in resources)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
