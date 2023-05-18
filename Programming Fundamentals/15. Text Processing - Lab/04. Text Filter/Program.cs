3159340158b363d7af3816a46ea1c5469dd86333
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-18 20:15:28 +0300

    added lab

---
+++ Programming Fundamentals/15. Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (string bannedWord in bannedWords)
+            {
+                string replacedWithAsteriks = new string('*', bannedWord.Length);
+
+                text = text.Replace(bannedWord, replacedWithAsteriks);
+            }
+
+            Console.WriteLine(text);
+
+        }
+    }
+}
