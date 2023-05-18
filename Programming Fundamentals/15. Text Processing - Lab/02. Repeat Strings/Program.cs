41229af15ef03c91e6ce09931cb0d4dd609f83c3
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-18 20:15:08 +0300

    added lab

---
+++ Programming Fundamentals/15. Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (string item in input)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    result.Append(item);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
