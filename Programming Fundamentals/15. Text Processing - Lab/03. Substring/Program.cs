725fea0731dbbf0e5e6fe6e6b86f9623ddb130aa
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-18 20:15:18 +0300

    added lab

---
+++ Programming Fundamentals/15. Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string substringToRemove = Console.ReadLine();
+            string input = Console.ReadLine();
+
+
+            while (input.Contains(substringToRemove))
+            {
+                int startIndex = input.IndexOf(substringToRemove);
+
+                input = input.Remove(startIndex, substringToRemove.Length);
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
