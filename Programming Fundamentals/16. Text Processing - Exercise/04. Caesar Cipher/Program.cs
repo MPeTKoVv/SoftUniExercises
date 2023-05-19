94f78c17dcf72eafa84d7dada2f694e3b426940d
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-20 00:21:51 +0300

    added exercise

---
+++ Programming Fundamentals/16. Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string cipheredText = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                cipheredText += (char)(input[i] + 3);
+            }
+
+            Console.WriteLine(cipheredText);
+        }
+    }
+}
