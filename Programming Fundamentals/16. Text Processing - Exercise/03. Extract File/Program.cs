0d8442aa2e9ea965df19eb8f55a3b940682b7a0b
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-20 00:21:44 +0300

    added exercise

---
+++ Programming Fundamentals/16. Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileDirectory = Console.ReadLine().Split("\\");
+
+            string[] file = fileDirectory[fileDirectory.Length - 1].Split('.');
+
+            string fileName = file[0];
+            string fileExtension = file[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
