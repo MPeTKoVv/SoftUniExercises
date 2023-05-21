aab78992206d07ef9da8c1cb191abccbb6b18e9c
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-21 11:10:45 +0300

    added lab

---
+++ Programming Fundamentals/17. Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\+359([ \-])2\1[0-9]{3}\1[0-9]{4}\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
