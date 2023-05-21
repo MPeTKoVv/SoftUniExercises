e9c6745cab6b65dd9cad1b272bff2c9ae51ff45b
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-21 11:10:51 +0300

    added lab

---
+++ Programming Fundamentals/17. Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\b(?<day>\d{2})(?<separator>[\-\.\/])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>\d{4})\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match date in matches)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
