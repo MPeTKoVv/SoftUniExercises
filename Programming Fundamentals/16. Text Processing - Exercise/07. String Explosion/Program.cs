21e60b5ebabf509b9d30a087b3383b532b02a43a
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-22 22:03:21 +0300

    added exercise

---
+++ Programming Fundamentals/16. Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string explosions = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i]=='>')
+                {
+                    explosions += text[i + 1];
+                }
+            }
+
+            for (int i = 0; i < explosions.Length; i++)
+            {
+                text = text.Remove((text[explosions[i]], explosions[i]));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
