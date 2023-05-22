169781881ccf1fbfd41b6232cee41b20524d90e9
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-22 22:03:14 +0300

    added exercise

---
+++ Programming Fundamentals/16. Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = "";
+
+            output += input[0];
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i - 1])
+                {
+                    output += input[i];
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
