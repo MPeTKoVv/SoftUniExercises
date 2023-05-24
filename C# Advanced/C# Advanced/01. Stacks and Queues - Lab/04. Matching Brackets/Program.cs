1ada731509f661d50dacef914d5860a2293bb605
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-24 21:07:22 +0300

    added lab

---
+++ C# Advanced/C# Advanced/01. Stacks and Queues - Lab/04. Matching Brackets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string contest = input.Substring(startIndex, i - startIndex + 1);
+
+                    Console.WriteLine(contest);
+
+                }
+
+            }
+        }
+    }
+}
