b128f8e1c0df9920c2c2a0197dee4ee5dbed6e2b
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-24 21:07:16 +0300

    added lab

---
+++ C# Advanced/C# Advanced/01. Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+
+                if (stack.Count == 3)
+                {
+                    int first = int.Parse(stack.Pop());
+                    var sign = stack.Pop();
+                    int second = int.Parse(stack.Pop());
+                    int result = 0;
+
+                    if (sign == "+")
+                    {
+                        result = first + second;
+                    }
+                    else if (sign == "-")
+                    {
+                        result = second - first;
+                    }
+
+                    stack.Push(result.ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
