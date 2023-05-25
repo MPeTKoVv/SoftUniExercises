2f5655a79d1a6c7bceea53d003eedbb36cbab4fc
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-25 13:15:39 +0300

    added lab

---
+++ C# Advanced/C# Advanced/01. Stacks and Queues - Lab/06. Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Paid")
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, names));
+                    names.Clear();
+                    continue;
+                }
+                else if (command == "End")
+                {
+                    Console.WriteLine($"{names.Count} people remaining.");
+                    return;
+                }
+
+                names.Enqueue(command);
+            }
+        }
+    }
+}
