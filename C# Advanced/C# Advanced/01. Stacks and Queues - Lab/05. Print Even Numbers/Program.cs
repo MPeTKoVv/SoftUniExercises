fc22a2b3ffc76e2be2ea88b2a2f485a9c05973f1
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-25 13:15:31 +0300

    added lab

---
+++ C# Advanced/C# Advanced/01. Stacks and Queues - Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> evenNums = new Queue<int>();
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenNums.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", evenNums));
+        }
+    }
+}
