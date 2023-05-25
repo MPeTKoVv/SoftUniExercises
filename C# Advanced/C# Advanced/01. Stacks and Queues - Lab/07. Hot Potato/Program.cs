8f8749af4e806170a71829b86d8b43b364f1cb50
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-25 13:15:48 +0300

    added lab

---
+++ C# Advanced/C# Advanced/01. Stacks and Queues - Lab/07. Hot Potato/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            int cnt = int.Parse(Console.ReadLine());
+
+            Queue<string> players = new Queue<string>(names);
+
+            while (players.Count > 1)
+            {
+                for (int i = 0; i < cnt - 1; i++)
+                {
+                    players.Enqueue(players.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {players.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {players.Peek()}");
+        }
+    }
+}
