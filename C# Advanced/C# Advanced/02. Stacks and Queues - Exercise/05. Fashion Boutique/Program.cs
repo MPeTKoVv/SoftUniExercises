82527a0e6c01fc91568f272fed580174684b5625
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-28 00:10:17 +0300

    added exercise

---
+++ C# Advanced/C# Advanced/02. Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+
+            Stack<int> clothesInBox = new Stack<int>(input);
+
+            int cntRacks = 0;
+
+            while (clothesInBox.Count > 0)
+            {
+                
+            }
+        }
+    }
+}
