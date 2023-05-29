806266fac78f9a2e81271dc19529c5aa942d722e
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-29 16:26:18 +0300

    added exercise

---
+++ C# Advanced/C# Advanced/02. Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = int.Parse(Console.ReadLine());
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < value; i++)
+            {
+                int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int amount = array[0];
+                int distance = array[1];
+
+                queue.Enqueue(amount);
+            }
+
+            Console.WriteLine(queue.Min());
+        }
+    }
+}
