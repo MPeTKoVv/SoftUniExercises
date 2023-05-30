18ddf8d0aeaea5b8b8e0559a81fa1363d08d7995
MPeTKoVv <petkov.martin19@gmail.com>
2023-05-30 23:00:49 +0300

    added lab

---
+++ C# Advanced/C# Advanced/03. Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] curRow = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = curRow[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
