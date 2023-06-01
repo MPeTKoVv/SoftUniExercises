bdde643132a6b136ff6a36114afc9862e365a20e
MPeTKoVv <petkov.martin19@gmail.com>
2023-06-02 00:23:17 +0300

    added exercise

---
+++ C# Advanced/C# Advanced/04. Multidimensional Arrays - Exercise/03. Maximal Sum/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Maximal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = size[0];
+            int cols = size[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            int maxSum = int.MinValue;
+            int maxSumRow = 0;
+            int maxSumCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int curSum = 0;
+
+                    curSum =
+                        matrix[row, col] +
+                        matrix[row + 1, col] + matrix[row + 2, col] +
+                        matrix[row, col + 1] + matrix[row, col + 2] +
+                        matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                        matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (curSum > maxSum)
+                    {
+                        maxSum = curSum;
+                        maxSumRow = row;
+                        maxSumCol = col;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = maxSumRow; row < maxSumRow + 3; row++)
+            {
+                for (int col = maxSumCol; col < maxSumCol + 3; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
