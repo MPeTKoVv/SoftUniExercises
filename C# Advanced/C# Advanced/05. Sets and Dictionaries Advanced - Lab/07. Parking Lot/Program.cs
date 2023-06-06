d587855061c5c662e97f79428a78044ccd1fe0d8
MPeTKoVv <petkov.martin19@gmail.com>
2023-06-06 23:43:36 +0300

    added lab

---
+++ C# Advanced/C# Advanced/05. Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> numbers = new HashSet<string>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(", ");
+                string command = input[0];
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string number = input[1];
+
+                if (command == "IN")
+                {
+                    numbers.Add(number);
+                }
+                else if (command == "OUT")
+                {
+                    numbers.Remove(number);
+                }
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, numbers));
+            }
+        }
+    }
+}
