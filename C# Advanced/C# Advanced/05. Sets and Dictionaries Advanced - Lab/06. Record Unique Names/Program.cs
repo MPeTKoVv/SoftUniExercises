f990e6a0a384c559af1c0b032deaee635079fb8a
MPeTKoVv <petkov.martin19@gmail.com>
2023-06-06 23:43:27 +0300

    added lab

---
+++ C# Advanced/C# Advanced/05. Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int cnt = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cnt; i++)
+            {
+                string currName = Console.ReadLine();
+
+                names.Add(currName);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
