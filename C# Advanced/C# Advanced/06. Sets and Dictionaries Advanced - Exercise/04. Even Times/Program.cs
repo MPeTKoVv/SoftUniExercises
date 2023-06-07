ebe0ac42d2d404d42e7f7efd2762bfe341305b16
MPeTKoVv <petkov.martin19@gmail.com>
2023-06-08 00:37:19 +0300

    added exercise

---
+++ C# Advanced/C# Advanced/06. Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < cnt; i++)
+            {
+                int currNum = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(currNum))
+                {
+                    numbers.Add(currNum, 0);
+                }
+
+                numbers[currNum]++;
+            }
+
+            foreach (var item in numbers)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
