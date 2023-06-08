ae3376808b05a550bae4440d5a1c2a807647f738
MPeTKoVv <petkov.martin19@gmail.com>
2023-06-09 00:01:29 +0300

    added lab

---
+++ C# Advanced/C# Advanced/07. Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] sorted = numbers.OrderByDescending(x => x).ToArray();
+
+            if (sorted.Length < 3)
+            {
+                for (int i = 0; i < sorted.Length; i++)
+                {
+                    Console.Write($"{sorted[i]} ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{sorted[i]} ");
+                }
+            }
+        }
+    }
+}
