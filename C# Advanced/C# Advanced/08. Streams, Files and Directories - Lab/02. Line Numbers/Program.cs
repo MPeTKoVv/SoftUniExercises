227efe78712cee3bf18ea4e6ceb83ab07e6b331e
MPeTKoVv <petkov.martin19@gmail.com>
2023-06-12 18:22:14 +0300

    added lab

---
+++ C# Advanced/C# Advanced/08. Streams, Files and Directories - Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int row = 1;
+
+                    while (!reader.EndOfStream)
+                    {
+                        writer.WriteLine($"{row++}. {reader.ReadLine()}");
+                    }
+                }
+            }
+        }
+    }
+}
