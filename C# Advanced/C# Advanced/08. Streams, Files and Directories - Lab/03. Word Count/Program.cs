610387eb5334198e8578846f94faeceebe6dca5f
MPeTKoVv <petkov.martin19@gmail.com>
2023-06-12 18:22:23 +0300

    added lab

---
+++ C# Advanced/C# Advanced/08. Streams, Files and Directories - Lab/03. Word Count/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(textFilePath))
+            {
+                var words = wordsFilePath.Split().ToList();
+
+                foreach (var word in words)
+                {
+                    int counter = 0;
+
+                    while (!reader.EndOfStream)
+                    {
+                        string[] curRow = reader.ReadLine().Split();
+
+                        foreach (var curWord in curRow)
+                        {
+                            if (curWord == word)
+                            {
+                                counter++;
+                            }
+                        }
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+                        writer.WriteLine($"{word} - {counter}");
+                    }
+                }
+            }
+        }
+    }
+}
