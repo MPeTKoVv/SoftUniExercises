4389018e10e46e6cb7fd72c19a55182a08b3e371
MPeTKoVv <petkov.martin19@gmail.com>
2023-06-13 12:34:58 +0300

    added exercise

---
+++ C# Advanced/C# Advanced/09. Streams, Files and Directories - Exercises/06. ZipAndExtracts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZipAndExtracts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
